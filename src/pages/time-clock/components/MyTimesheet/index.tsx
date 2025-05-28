137b7e60771837c20373fea8b12beec4f2f58b57
An <140888823+ancJ29@users.noreply.github.com>
2025-05-28 17:34:14 +0700

    sync@20250528.1734

--- src/pages/time-clock/components/MyTimesheet/index.tsx
+++ src/pages/time-clock/components/MyTimesheet/index.tsx
@@ -1,3 +1,3 @@
 export default function MyTimesheet() {
-  return <>My timesheet</>
+  return <></>
 }
--- src/pages/time-clock/components/TimeOffCenter/index.tsx
+++ src/pages/time-clock/components/TimeOffCenter/index.tsx
@@ -1,3 +1,3 @@
 export default function TimeOffCenter() {
-  return <>My timesheet</>
+  return <></>
 }
